1d687c5fc2d63eda1e1079144ae98268e63ae5bd
[Rahul Sethi] <[rahuls14@illinois.edu]>
2025-10-03 05:05:57 -0500

    deploying

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { PokemonProvider } from './context/PokemonContext';
 import './App.css';
 
 const App: React.FC = () => (
-  <BrowserRouter basename="/mp2">
+  <BrowserRouter basename="kantopokedex">
     <PokemonProvider>
       <Layout>
         <Routes>
